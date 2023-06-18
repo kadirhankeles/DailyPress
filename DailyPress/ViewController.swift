06a13429a10f617ca117d1cf2af6a18de3035242
Kadirhan Keleş <kadirhankeles@gmail.com>
2023-06-18 19:23:07 +0300

    Initial Commit

---
+++ DailyPress/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyPress
+//
+//  Created by Kadirhan Keles on 18.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
